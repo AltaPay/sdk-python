e20f2e14a2cd4b8fd2fb0a89b84fc4cd48e1b463
Bushra Asif <bushraasif931@gmail.com>
2026-04-28 19:31:31 +0500

    Fix import order for CheckoutSession in __init__.py

--- altapay/__init__.py
+++ altapay/__init__.py
@@ -10,8 +10,8 @@ __api_base_url__ = {
 
 from altapay.api import API  # NOQA
 from altapay.callback import Callback  # NOQA
-from altapay.checkout_session import CheckoutSession  # NOQA
 from altapay.chargeback import ChargebackEvent  # NOQA
+from altapay.checkout_session import CheckoutSession  # NOQA
 from altapay.credit_card_wallet_initiate_app_payment import \
     CreditCardWalletInitiateAppPayment  # NOQA
 from altapay.funding import CustomReport, Funding, FundingList  # NOQA
